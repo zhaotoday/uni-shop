7d206f85e12b8b1148865d6a1213ce5cea668d56
zhaojintian <729234283@qq.com>
2022-06-14 14:51:37 +0800

    u

--- composables/use-products.js
+++ composables/use-products.js
@@ -1,9 +1,13 @@
+import { formatNumber } from "jt-helpers";
+
 export const useProducts = () => {
   const getTotalPrice = (products) => {
     if (products.length) {
-      return products
-        .map(({ price, number }) => price * number)
-        .reduce((prev, current) => prev + current);
+      return formatNumber(
+        products
+          .map(({ price, number }) => price * number)
+          .reduce((prev, current) => prev + current)
+      );
     } else {
       return 0;
     }
